f45f91079841000b1a085209f5b847d6a2e12d4b
rahithyakilaru <rahithyakilaru9@gmail.com>
2025-03-04 02:00:32 -0600

    DrumsProject

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-    var randomNumber1 = Math.floor(Math.random()*6)+1;
-    var randomNumber2 = Math.floor(Math.random()*6)+1;
-    var diceNumber1 = "dice"+ randomNumber1 + ".png";
-    var diceNumber2 = "dice"+ randomNumber2 +".png"
-    document.querySelector("img").setAttribute("src","images/"+diceNumber1);
-    document.querySelectorAll("img")[1].setAttribute("src","images/"+diceNumber2);
+var drum_click = document.querySelectorAll(".drum");
 
-    if (randomNumber1> randomNumber2) {
-        document.firstElementChild.lastElementChild.querySelector("h1").innerHTML="🚩Player 1 Wins";
-    } else if (randomNumber1< randomNumber2){
-        document.firstElementChild.lastElementChild.querySelector("h1").innerHTML="Player 2 Wins🚩";
-    } else{
-        document.querySelector("h1").innerHTML="Draw";
+for (i=0; i<drum_click.length;i++){
+    drum_click[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+        
+    })
+}
+document.addEventListener("keydown", function(event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+})
+function makesound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+            case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+            case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+            case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+            case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+            case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    
+        default:
+            break;
     }
+}
+function buttonAnimation(event){
+    var animationKey= document.querySelector("."+event);
+    animationKey.classList.add("pressed");
+    setTimeout(function(){
+        animationKey.classList.remove("pressed");
+    },100);
+}
